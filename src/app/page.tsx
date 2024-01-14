5033c298815c452f4b44e82354ba2a81b654a6bc
jdwilkin4 <jwilkin4@hotmail.com>
2024-01-14 03:19:46 -0800

    Revert "fix: as attribute"
    This reverts commit 3dd6d34b04ad842eeb6cadcd788439780fd6c461.
    
    reverting prevous changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
 
       <div className="flex flex-wrap justify-center align-center gap-8">
         {LIST_OF_PROJECTS.map(({ title, tags, route }) => (
-          <Link href="/[slug]/page" key={title} as={`/${route || ""}/page`}>
+          <Link href="/[slug]/page" key={title} as={route ? `/${route}` : "/"}>
             <Card key={title} title={title} tags={tags || []} />
           </Link>
         ))}

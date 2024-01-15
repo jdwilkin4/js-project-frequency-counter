0617c7ca5716a0b26aa488aa3cbcb9c7deafbe30
jdwilkin4 <jwilkin4@hotmail.com>
2024-01-14 18:24:31 -0800

    fix: routes

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -2,6 +2,8 @@ import { Table } from "@/components/Table";
 import { LIST_OF_PROJECTS } from "../../../utils/constants";
 import { HomeLink } from "@/components/HomeLink";
 
+export const dynamicParams = true;
+
 export default function Page({ params }: { params: { slug: string } }) {
   const projectRoutes = LIST_OF_PROJECTS.map(({ route }) => route);
   const projectIndex = projectRoutes.indexOf(`${params.slug}`);
